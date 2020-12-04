0521641667c7cc1abd35342c49c010c5478245ca
Ira-20 <gupta.ira12@gmail.com>
2020-12-04 16:56:20 +0530

    second commit

---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentApp.Models
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        public int SId { get; set; }
+        public string Sname { get; set; }
+        public DateTime SDOB { get; set; }
+        public double Contact { get; set; }
+
+    }
+}
